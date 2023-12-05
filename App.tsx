0151d163c0a146e62bca73becf2cdee2f842a964
khoirulmuasin <gabutcrypto1@gmail.com>
2023-12-05 19:06:46 +0700

    class component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Image, Text, TextInput, View } from "react-native";
 
 const App = () => {
@@ -10,7 +10,10 @@ const App = () => {
       <Text>Halo2</Text>
       <Foto />
       <Home />
+      <Nama />
       <TextInput style={{ borderWidth: 2 }} />
+      <BoxGreen />
+      <Profile />
     </View>
   );
 };
@@ -22,6 +25,28 @@ const Home = () => {
 const Foto = () => {
   return <Image source={{ uri: 'https://cdn.oneesports.id/cdn-data/sites/2/2022/03/naruto-uzumaki-1-1024x653.jpg' }}
     style={{ width: 100, height: 100, }} />
+};
+
+const Nama = () => {
+  return <Text>Nama Ku AAA</Text>
+};
+
+class BoxGreen extends Component {
+  render() {
+    return <Text style={{ marginLeft: 10, marginTop: 10 }}>Ini Class</Text>
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={{ uri: 'https://cdn0-production-images-kly.akamaized.net/FTOfaG5coODpnvPfVcka2r4P5V4=/1200x900/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3167999/original/084845700_1593662619-Template_Landscape_1_.jpg' }}
+          style={{ width: 100, height: 100, borderRadius: 50 }} />
+        <Text style={{ color: 'red', fontSize: 30, marginHorizontal: 10 }}>Ini Siapa</Text>
+      </View>
+    )
+  }
 }
 
 export default App;
